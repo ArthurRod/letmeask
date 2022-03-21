add2f24355a46329a97c4c5eb1b27941524b5801
ArthurRod <arthur9713@hotmail.com>
2022-03-21 17:56:07 -0300

    First Commit

---
+++ src/components/Question/index.tsx
@@ -0,0 +1,42 @@
+import { ReactNode } from 'react';
+import cx from 'classnames';
+
+import './styles.scss';
+
+type QuestionProps = {
+  content: string;
+  author: {
+    name: string;
+    avatar: string;
+  };
+  children?: ReactNode; //ReactNode é qualquer conteúdo jsx texto, div, component etc... 
+  isAnswered?: boolean;
+  isHighlighted?: boolean;
+}
+
+export function Question({
+  content,
+  author,
+  isAnswered = false,
+  isHighlighted = false,
+  children,
+}: QuestionProps) {
+  return (
+    <div 
+      className={cx(
+        'question', 
+        { answered: isAnswered },
+        { highlighted: isHighlighted && !isAnswered },
+      )}
+    >
+      <p>{content}</p>
+      <footer>
+        <div className="user-info">
+          <img src={author.avatar} alt={author.name} />
+          <span>{author.name}</span>
+        </div>
+        <div>{children}</div>
+      </footer>
+    </div>
+  );
+}
